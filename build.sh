0f2878b03d437c66d66c3b4916ddedd2fbeb160d
Ruben <15885455+RubenNL@users.noreply.github.com>
2021-04-30 16:04:50 +0200

    add multi-build build.sh

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker build -t docker.rubend.nl/statuspage-frontend -f Frontend .
+docker push docker.rubend.nl/statuspage-frontend
+docker buildx build -f Backend --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/s390x --push --tag docker.rubend.nl/statuspage:latest .
+docker rmi docker.rubend.nl/statuspage-frontend
