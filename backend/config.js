3aa333fe82083a8c8757c347ca4f5810f799b017
Ruben <15885455+RubenNL@users.noreply.github.com>
2021-04-30 13:32:28 +0200

    demo config.

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,11 @@
 ({dig,port}=require('./tasks/index.js'))
 module.exports=[
-	
+	{promise:dig('7.7.7.7','google.com'),name:"dns check 7.7.7.7 for google.com(should fail)",after:[
+		{promise:dig('123.123.123.123','google.com'),name:"shouldnt execute"},
+	]},
+	{promise:port('google.com','80'),name:'port 80 on google.com(SHOULD SUCCEED)',after:[
+		{promise:()=>new Promise((resolve,reject)=>setTimeout(resolve,10000)),name:"10 sec timeout test", after:[
+			{promise:dig('8.8.8.8','google.com'),name:"should execute after 10 seconds and succeed"},
+		]},
+	]},
 ]
