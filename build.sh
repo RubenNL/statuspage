b32430d8441288a1d852ebb05b004eb475be8f6e
Ruben <15885455+RubenNL@users.noreply.github.com>
2021-04-30 17:59:53 +0200

    change multi build config.

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-docker build -t docker.rubend.nl/statuspage-frontend -f Frontend .
-docker push docker.rubend.nl/statuspage-frontend
+docker build -t docker.rubend.nl/statuspage-build -f AllArch .
+docker push docker.rubend.nl/statuspage-build
 docker buildx create --use
-docker buildx build -f Backend --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/s390x --push --tag docker.rubend.nl/statuspage:latest .
-docker rmi docker.rubend.nl/statuspage-frontend
+docker buildx build -f EveryArch --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/s390x --push --tag docker.rubend.nl/statuspage:latest .
