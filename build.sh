ad1e9d9f6993c2462d2bcefcb381a68797f291a7
Ruben <15885455+RubenNL@users.noreply.github.com>
2021-04-30 16:46:35 +0200

    docker buildx create use

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker build -t docker.rubend.nl/statuspage-frontend -f Frontend .
 docker push docker.rubend.nl/statuspage-frontend
+docker buildx create --use
 docker buildx build -f Backend --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/s390x --push --tag docker.rubend.nl/statuspage:latest .
 docker rmi docker.rubend.nl/statuspage-frontend
