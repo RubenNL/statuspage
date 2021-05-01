97397134ee6304aa7f6bf9f61b00c7d44b8c85ba
Ruben <15885455+RubenNL@users.noreply.github.com>
2021-05-01 09:53:50 +0200

    fix cross-platform build

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-docker build -t docker.rubend.nl/statuspage-build -f AllArch .
-docker push docker.rubend.nl/statuspage-build
+docker buildx build --push -t docker.rubend.nl/statuspage-build -f AllArch .
 docker buildx create --use
 docker buildx build -f EveryArch --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/s390x --push --tag docker.rubend.nl/statuspage:latest .
