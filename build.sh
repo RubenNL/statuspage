10610fb9b9e90fdc3802c36d1492971b2cbfb982
Ruben <15885455+RubenNL@users.noreply.github.com>
2021-05-01 11:07:49 +0200

    remove buildx runner after push

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 docker buildx build --push -t docker.rubend.nl/statuspage-build -f AllArch .
 docker buildx create --use
 docker buildx build -f EveryArch --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/s390x --push --tag docker.rubend.nl/statuspage:latest .
+docker buildx stop
+docker buildx rm
