781139327b9536ffd51ffd78c0c478bd9b208d63
Ruben <15885455+RubenNL@users.noreply.github.com>
2021-05-03 10:59:28 +0200

    http(s) get

---
+++ backend/modules/httpget.js
@@ -0,0 +1,22 @@
+module.exports=function({url}) {
+	const module=url.split(':')[0]=="http"?require('http'):require('https');
+	const call=()=>new Promise((resolve,reject)=>{
+		module.get(url,res=>{
+			data='';
+			res.on('data',chunk=>data+=chunk);
+			res.on('end',()=>{
+				if(res.statusCode==200) resolve(data);
+				else reject({statusCode:res.statusCode,data})
+			})
+		})
+	})
+	return {
+		call,
+		name: `http(s) get ${url}`,
+	}
+}
+module.exports.help={
+	url:"url to get",
+}
+module.exports.info="http get request(also supports https)"
+ 
