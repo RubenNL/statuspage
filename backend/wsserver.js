5d00df4a7fc8c66aa550eeccff6491166f79d3a9
Ruben <15885455+RubenNL@users.noreply.github.com>
2021-05-07 11:29:40 +0200

    move function outside main function

--- backend/wsserver.js
+++ backend/wsserver.js
@@ -1,23 +1,23 @@
 const modules=require('./modules');
 const WebSocket = require('ws');
 const {getConfig}=require('./configinit')
-module.exports=server=>(new WebSocket.Server({ server })).on('connection', function connection(ws) {
-	function getParsedConfig() {
-		function parseActions(actions,trace) {
-			return actions.map((action,id) => {
-				if(!action.data) action.data = modules[action.module](action.args,data.moduleConfig[action.module]);
-				if(action.after) action.after=parseActions(action.after,[...trace,id]);
-				else action.after=[];
-				action.trace=JSON.stringify([...trace,id]);
-				action.status="PENDING";
-				action.childError=false;
-				return action;
-			})
-		}
-		const data=getConfig();
-		data.actions=parseActions(data.actions,[]);
-		return data;
+function getParsedConfig() {
+	function parseActions(actions,trace) {
+		return actions.map((action,id) => {
+			if(!action.data) action.data = modules[action.module](action.args,data.moduleConfig[action.module]);
+			if(action.after) action.after=parseActions(action.after,[...trace,id]);
+			else action.after=[];
+			action.trace=JSON.stringify([...trace,id]);
+			action.status="PENDING";
+			action.childError=false;
+			return action;
+		})
 	}
+	const data=getConfig();
+	data.actions=parseActions(data.actions,[]);
+	return data;
+}
+module.exports=server=>(new WebSocket.Server({ server })).on('connection', function connection(ws) {
 	const config=getParsedConfig();
 	ws.oldSend=ws.send;
 	ws.send=data=>ws.oldSend(JSON.stringify(data));
@@ -31,11 +31,11 @@ module.exports=server=>(new WebSocket.Server({ server })).on('connection', funct
 	function sendStatus(trace,status,response) {
 		ws.send({type:"status",trace,status,response})
 	}
-	function doActionList(actions,outsideTrace,response) {
+	function doActionList(actions,outsideTrace,input) {
 		actions=actions.map((task,id)=>{
 			const trace=[...outsideTrace,id];
 			sendStatus(trace,"STARTED","active...")
-			return task.data.call(response).then(response=>{
+			return task.data.call(input).then(response=>{
 				sendStatus(trace,"SUCCESS",response)
 				return task.after?doActionList(task.after,trace,response):null;
 			},err=>{
